b8b600b40ea95934edb68ec4fc4688c48fe41b84
Daniel Caldas <daniel.lc.caldas@outlook.com>
2025-07-27 13:20:19 -0300

    dashboard initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ExampleModule } from './features/example/example.module';
 import { provideHttpClient } from '@angular/common/http';
 import { TransferModule } from './features/transfer/transfer.module';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, ExampleModule, TransferModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
