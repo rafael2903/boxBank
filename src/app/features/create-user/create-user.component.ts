75914f3a6b797ffa9635de670f072103b37508a0
Jose Soares <joserisoares2001@gmail.com>
2025-07-30 17:32:15 -0300

    Sing in feature

--- src/app/features/create-user/create-user.component.ts
+++ src/app/features/create-user/create-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../shared/services/users.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../shared/models/user.model';
 
 @Component({
   selector: 'app-create-user',
@@ -7,12 +11,17 @@ import { Component } from '@angular/core';
   styleUrl: './create-user.component.scss',
 })
 export class CreateUserComponent {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
   nome: string = '';
   email: string = '';
   cpf: string = '';
   senha: string = '';
   confirmarSenha: string = '';
-
   mensagemSucesso: string = '';
   mensagemErro: string = '';
 
@@ -29,5 +38,30 @@ export class CreateUserComponent {
       this.mensagemErro = 'A senha e a confirmação de senha não coincidem.';
       return;
     }
+
+    const novoUsuario: User = {
+      cpf: this.cpf,
+      email: this.email,
+      name: this.nome,
+      password: this.senha,
+      amount: 1000,
+      id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+    };
+
+    this.usersService.create(novoUsuario).subscribe({
+      next: () => {
+        this.authService.login(this.email, this.senha).subscribe((user) => {
+          if (user) {
+            this.mensagemSucesso = 'Usuário cadastrado e logado com sucesso!';
+            this.router.navigate(['/dashboard']);
+          } else {
+            this.mensagemErro = 'Erro ao realizar login automático.';
+          }
+        });
+      },
+      error: (error) => {
+        this.mensagemErro = 'Erro ao cadastrar usuário: ' + error.message;
+      }
+    });
   }
 }
--- src/app/features/create-user/create-user.module.ts
+++ src/app/features/create-user/create-user.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { CreateUserComponent } from './create-user.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [CreateUserComponent],
   imports: [CommonModule, SharedModule, FormsModule, RouterLink],
+  providers: [provideHttpClient()],
+
 })
 export class CreateUserModule {}
